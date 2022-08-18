6b8c8f272f8bb06be227f213e292ac70dd3a35da
amalazza <dinaazza8@gmail.com>
2022-08-18 14:57:41 +0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <app-nav-bar />
   </div>
 </template>
+
+<script>
+import navBar from "@/components/Nav.vue";
+
+export default {
+  components: {
+    "app-nav-bar": navBar,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
